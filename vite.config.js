f515f7e7cb46370293a1c58169983291beb6493a
Ramganga Kumar <134604356+ramganga911@users.noreply.github.com>
2024-07-20 05:47:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+import { defineConfig } from 'vite';
+
 export default defineConfig({
-  plugins: [react()],
-  base:"/daily-news/",
+  server: {
+    host: '0.0.0.0', // This makes the server accessible externally
+    port: 3000,      // You can choose a specific port
+  },
 })
